db345a8fcffb46e0379609006e10999bd6e70e56
Leonard Aukea <leonard.aukea@volvocars.com>
2019-09-02 15:49:30 +0200

    updated scipy imgae

--- pipeline-components/notebook/src/build.sh
+++ pipeline-components/notebook/src/build.sh
@@ -12,6 +12,10 @@ CONTAINER_NAME=papermill
 
 docker build -t ${CONTAINER_NAME} .
 docker tag ${CONTAINER_NAME} quay.io/volvocars/${PROJECT_ID}:${CONTAINER_NAME}${TAG_NAME}
-docker push quay.io/${PROJECT_ID}/${CONTAINER_NAME}:${TAG_NAME}
+docker push quay.io/volvocars/${PROJECT_ID}:${CONTAINER_NAME}${TAG_NAME}
 # Output the strict image name (which contains the sha256 image digest)
 docker inspect --format="{{index .RepoDigests 0}}" "${IMAGE_NAME}"
+
+
+
+quay.io/volvocars/jupyter:scipy-notebook:latest
